ff8d5046af1444ebd7d37a50fa0e4525ecb667bf
fuad <fuad221@outlook.com>
2021-06-30 15:30:28 +0200

    i deleted the HeaderUsers and i put the router in the App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 
 import React from "react";
-// import Register from './components/users/Registration'
-import HeaderUsers from './components/users/HeaderUsers'
-// import "./App.css";
-// //import Navbar from './components/Navbar.js'
-// import {
-//   Route,
-//   NavLink,
-//   BrowserRouter as Router,
-//   Switch,
-// } from "react-router-dom";
-// import Login from "./components/users/Login";
-// import { Button } from "@material-ui/core";
+import Register from './components/users/Registration'
+// import HeaderUsers from './components/users/HeaderUsers'
+import "./App.css";
+//import Navbar from './components/Navbar.js'
+import {
+  Route,
+  NavLink,
+  BrowserRouter as Router,
+  Switch,
+} from "react-router-dom";
+import Login from "./components/users/Login";
+import { Button } from "@material-ui/core";
 
 function App() {
   return (
     <div>
-      <HeaderUsers />
+      {/* <HeaderUsers /> */}
+      <Router>
+        <Button component={NavLink} to="/login">
+          Login
+        </Button>
+  
+  
+        <Button component={NavLink} to="/register">
+          Register
+        </Button>
+  
+  
+        <Switch>
+          <Route exact path="/login" render={props => <Login {...props} />} />
+          <Route exact path="/register" render={props => <Register {...props} />} />
+        </Switch>
+      </Router>
+
     </div>
   )
 }
