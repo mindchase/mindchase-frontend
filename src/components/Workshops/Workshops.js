7b0a3ba54716125791292bec7973fde62cd4b2ee
Paul <you@example.com>
2021-07-06 10:06:03 +0200

    added workshops comp and branch

---
+++ src/components/Workshops/Workshops.js
@@ -0,0 +1,25 @@
+import React from "react";
+import WorkshopItem from "./WorkshopItem";
+import WorkshopData from "./WorkshopData";
+import { Box, Grid } from "@material-ui/core";
+
+const Workshops = () => {
+  return (
+    <>
+      <Box p={5}>
+        <Grid container spacing={4}>
+          {/* loop them props */}
+          {WorkshopData.map((card, id) => {
+            return (
+              <Grid item xs={12} sm={6} md={4} key={id}>
+                <WorkshopItem {...card} />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </Box>
+    </>
+  );
+};
+
+export default Workshops;
