a091ec45f391c954df229cea99b02499199515db
Paul <you@example.com>
2021-07-27 15:36:06 +0200

    removed duplicate browserRouter once more

--- src/App.js
+++ src/App.js
@@ -1,94 +1,101 @@
 import Register from "./components/users/Registration";
 import ForgotPassword from "./components/users/ForgotPassword";
 //import NewPassword from "./components/users/NewPassword";
-import { Route, BrowserRouter as Router, Switch ,Redirect} from "react-router-dom";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 import Login from "./components/users/Login";
 import Fees from "./components/pages/Fees";
-import { ThemeProvider, CssBaseline ,createTheme} from "@material-ui/core";
+import { ThemeProvider, CssBaseline, createTheme } from "@material-ui/core";
 import HomeBanner from "./components/HomeBanner/HomeBanner";
 import Navbar from "./components/Navbar/Navbar";
 import FooterComponent from "./components/FooterComponent/FooterComponent";
-import Workshop from "./components/Workshops/WorkshopBody"
-import useToken from './components/useToken';
-import Welcome from './components/pages/Welcome'
+import Workshop from "./components/Workshops/WorkshopBody";
+import useToken from "./components/useToken";
+import Welcome from "./components/pages/Welcome";
 import VideoPlayer from "./components/video/VideoPlayer";
-import Chat from './components/chat/Chat'
-import Quiz from './components/quiz/Quiz'
+import Chat from "./components/chat/Chat";
+import Quiz from "./components/quiz/Quiz";
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#F0E7E1',
-      light:'#ffe082'
+      main: "#F0E7E1",
+      light: "#ffe082",
     },
-    secondary :{
-      main :'#FF1616'
-    }  
+    secondary: {
+      main: "#FF1616",
+    },
   },
-})
+});
 
 const App = () => {
   const { token, setToken } = useToken();
   return (
-    <>
-      <Router>
-        <ThemeProvider theme={theme} >
+    <Router>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
-           <Navbar token={token} setToken={setToken}   />
-     
-          <Switch>
-            <Route  path="/"
-                     exact
-                      component={HomeBanner} />
-            
-            <Route  path="/welcome" 
-                      component={Welcome} />
+        <Navbar token={token} setToken={setToken} />
 
-              <Route path="/login" 
-                render={(props) => <Login {...props} setToken={setToken} />}  />
+        <Switch>
+          <Route path="/" exact component={HomeBanner} />
 
-              <Route path="/fees" 
-                render={(props) => <Fees {...props}   />} />
+          <Route path="/welcome" component={Welcome} />
 
-              <Route path="/register"
-                render={(props) => <Register {...props} setToken={setToken} /> }/>
-
-              <Route  path="/forgotpassword"
-                render={(props) => <ForgotPassword {...props} setToken={setToken} />}/>
-
-             
-              <Route  path="/about" 
-                 redirect="/FooterComponents"/>   
-
-            {token  ? <>
-                <Route path="/workshops"
-                render={ (props) => <Workshop {...props} setToken ={setToken}  />  } 
-                component={Workshop} />
-                    <Route path="/Quiz"
-                render={ (props) => <Quiz {...props} setToken ={setToken}  />  }  />
-
-
-                  <Route path="/video-player" 
-                  render={ (props) => <VideoPlayer {...props} setToken ={setToken}  />  } 
+          <Route
+            path="/login"
+            render={(props) => <Login {...props} setToken={setToken} />}
           />
 
-                <Route  path="/messages" 
-                  render={ (props) => <Chat {...props} setToken ={setToken}  />  } /> 
-              </>  :
-              <Redirect to ='/welcome' />
-            }  
-                   
+          <Route path="/fees" render={(props) => <Fees {...props} />} />
 
+          <Route
+            path="/register"
+            render={(props) => <Register {...props} setToken={setToken} />}
+          />
 
-              
-          </Switch>
-         
-        </ThemeProvider>
-      <footer>
-       
-      </footer>
-      </Router>
-    </>
+          <Route
+            path="/forgotpassword"
+            render={(props) => (
+              <ForgotPassword {...props} setToken={setToken} />
+            )}
+          />
+
+          <Route path="/about" redirect="/FooterComponents" />
+
+          {token ? (
+            <>
+              <Route
+                path="/workshops"
+                render={(props) => <Workshop {...props} setToken={setToken} />}
+                component={Workshop}
+              />
+              <Route
+                path="/Quiz"
+                render={(props) => <Quiz {...props} setToken={setToken} />}
+              />
+
+              <Route
+                path="/video-player"
+                render={(props) => (
+                  <VideoPlayer {...props} setToken={setToken} />
+                )}
+              />
+
+              <Route
+                path="/messages"
+                render={(props) => <Chat {...props} setToken={setToken} />}
+              />
+            </>
+          ) : (
+            <Redirect to="/welcome" />
+          )}
+        </Switch>
+      </ThemeProvider>
+    </Router>
   );
 };
 
