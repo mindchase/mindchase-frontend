71b2f049c875b38557e155ba7aeacecefd1d3a21
Ibrahim  Andraws <55102557+Iandraws@users.noreply.github.com>
2021-07-29 09:39:42 +0200

    Reg Fixed Add Accu to WorshopBody

--- src/components/users/Registration.js
+++ src/components/users/Registration.js
@@ -1,25 +1,49 @@
 import React from 'react'
 import {useForm, Controller} from 'react-hook-form'
-import {Grid , Paper,TextField,Typography ,Button,Avatar,Link, Input} from '@material-ui/core'
+import { useHistory } from "react-router-dom";
+import { useState } from "react";
+import {Grid , Paper,TextField,Typography ,Button,Avatar,Link, } from '@material-ui/core'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
+import { hashPassword } from "./crypto";
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close'
+
 import axios from 'axios'
 
 const Registration=()=>{
-    const { register, handleSubmit, formState: { errors } , watch, control } = useForm()
+    //fun Sucsess Signup
+    const [open, setOpen] = React.useState(false);
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+          return;
+        }
+    
+        setOpen(false);
+      };
+
+    let history = useHistory();
+    const {
+        control,
+        handleSubmit,
+        formState: { errors },
+      } = useForm();
+      const [wrongInput, setWrongInput] = useState(false);
    
-    const submit = (data) => {
-        console.log("submit")
-        axios
-        .post(`${process.env.REACT_APP_API}/users/register`, data)
-        .then((response) => {
-                    console.log(response)
-        })
-        .catch((error) => {
-            console.log(errors)
-        })   
-    }
+      const submit = async (data) => {
+       
+        try {
+          const result = await axios.post("http://localhost:3001/users/", data);
+          if (result.status === 200) {
+            setOpen(true);
+            console.log(data);
+          }
+        } catch (e) {
+          console.log("@", e);
+          setWrongInput(e);
+          // showMessage();
+        }
+      };
 
     const paperStyle={padding :25,height:'78vh',width:310, margin:"180px auto"}
     const avatarStyle={backgroundColor:'#1bbd7e'}
@@ -36,87 +60,109 @@ const Registration=()=>{
                 </Grid>
 
                 <Controller
-                    name="name"
-                    control={control}
-                    defaultValue=""
-                    rules={{required: true}}
-                    render={({ field }) => <Input {...field} fullWidth />}
-                    placeholder='Enter username'
-                />
+                name="name"
+                control={control}
+                defaultValue=""
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    display="inline"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    label="name"
+                    autoComplete="name"
+                    autoFocus
+                  />
+                )}
+              />
+              {errors.firstName && <span>First name is required</span>}
+               <Controller
+                name="email"
+                control={control}
+                defaultValue=""
+                rules={{
+                  required: true,
+                  pattern: {
+                    value: /\S+@\S+\.\S+/,
+                    message: "Entered value does not match email format",
+                  },
+                }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    display="flex"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    label="Email Address"
+                    autoComplete="email"
+                    autoFocus
+                  />
+                )}
+              />
+
+              {errors.email && <span>{errors.email.message}</span>}
+
+              <Controller
+                name="password"
+                control={control}
+                defaultValue=""
+                rules={{
+                  required: true,
+                  pattern: {
+                    value:
+                      /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+                    message:
+                      "Password should contain small character,big character,special character,a number and needs to be at least 8 characters long",
+                  },
+                }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    display="flex"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    label="Password"
+                    type="password"
+                    autoFocus
+                  />
+                )}
+              />
+              {errors.password && <span>{errors.password.message}</span>}
+
+    
+
                  <Controller
-                    name="email"
-                    control={control}
-                    defaultValue=""
-                    rules={{...register("email", {required: true, pattern: /^\S+@\S+$/i})}}
-                    render={({ field }) => <Input {...field} fullWidth />}
-                    placeholder='Enter email'
-                    type='email'
-                />
-
+                name="retype-password"
+                control={control}
+                defaultValue=""
+                rules={{
+                  required: true,
+                  pattern: {
+                    value:
+                      /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+                    message:
+                      "Password should contain small character,big character,special character,a number and needs to be at least 8 characters long",
+                  },
+                }}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    display="inline-flex"
+                    variant="outlined"
+                    margin="normal"
+                    fullWidth
+                    label="Retype-Password"
+                    type="password"
+                    autoFocus
+                  />
+                )}
+              />
+              {errors.password && <span>{errors.password.message}</span>}
               
-                <TextField 
-                label='Password' 
-                placeholder='Enter password' 
-                type='password' 
-                fullWidth required
-                {...register("password", {required: true, minLength: 8})} 
-                />
-                {errors.password && <div>Password is required</div>}
-
-
-                <TextField label='Password repeat' 
-                placeholder='rPassword repeat' 
-                type='password' 
-                fullWidth required
-               {...register("password_repeat", {validate: (value) => {
-                return value === watch('password'); // value is from password_repeat and watch will return value from password
-                }})} 
-                />
-                  {errors.password_repeat && <div>Password is not the same</div>}
-                <FormControlLabel control={
-                        <Checkbox
-                            name="checkedB"
-                            color="primary"
-                        />
-                }
-                label="Remember me" /> 
-
-
-
-            
-                {/* <TextField label='Username'  fullWidth required
-                    />
-   <TextField label='your Email please' placeholder='Enter Email' 
-                {...register("email", {required: true, pattern: /^\S+@\S+$/i})}
-                fullWidth required/>
-             
-
-                <TextField 
-                label='Password' 
-                placeholder='Enter password' 
-                type='password' 
-                fullWidth required
-                {...register("password", {required: true, minLength: 8})} 
-                />
-                {errors.password && <div>Password is required</div>}
-
-
-                <TextField label='Password repeat' 
-                placeholder='rPassword repeat' 
-                type='password' 
-                fullWidth required
-               {...register("password_repeat", {validate: (value) => {
-                return value === watch('password'); // value is from password_repeat and watch will return value from password
-                }})} 
-                />
-                  {errors.password_repeat && <div>Password is not the same</div>}
-                <FormControlLabel control={
-                        <Checkbox
-                            name="checkedB"
-                            color="primary"
-                        />
-                }
-                label="Remember me" /> */}
                 
                 <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign Up</Button>
 
@@ -126,8 +172,34 @@ const Registration=()=>{
             </Paper>
             
     </Grid>
+    <div>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClose}
+        message="Sucsses Signed Up Plz Log In"
+        action={
+          <React.Fragment>
+            {/* <Button color="secondary" size="small" onClick={handleClose}>
+              UNDO
+            </Button> */}
+            <IconButton size="small" aria-label="close" color="inherit" onClick={handleClose}>
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </React.Fragment>
+        }
+      />
+    </div>
     </form>
+    
     )
+//fun Mess
+
+
 }
 
 export default Registration
